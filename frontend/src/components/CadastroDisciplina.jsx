dde152dfa3efe133efcb9597f1a0fcc80fd3dbc5
CarlosGbr11 <carlos157759@gmail.com>
2024-06-03 20:13:21 -0300

    Create CadastroDisciplina.jsx

---
+++ frontend/src/components/CadastroDisciplina.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CadastroDisciplina = () => {
+
+  return (
+    <div>
+      <h1>Cadastro de Disciplina</h1>
+    </div>
+  );
+}
+
+export default CadastroDisciplina;
