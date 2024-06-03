f6d7425d2d6e6b4554edc3fb6c5c1cd08205ca64
CarlosGbr11 <carlos157759@gmail.com>
2024-06-03 20:13:19 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,12 @@ import Home from './pages/Home';
 import Header from './pages/Header';
 import Footer from './pages/Footer';
 import CadastroProfessor from './components/CadastroProfessor';
+import CadastroDisciplina from './components/CadastroDisciplina';
+import CadastroSala from './components/CadastroSala';
+import CadastroTurma from './components/CadastroTurma';
+import Visualizar from './components/Visualizar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 
 function App() {
@@ -18,6 +23,10 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/Cadastro_professor' element={<CadastroProfessor />} />
+            <Route path='/Cadastro_disciplina' element={<CadastroDisciplina />} />
+            <Route path='/Cadastro_sala' element={<CadastroSala />} />
+            <Route path='/Cadastro_turma' element={<CadastroTurma />} />
+            <Route path='/Visualizar_cadastros' element={<Visualizar />} />
           </Routes>
         </main>
 
