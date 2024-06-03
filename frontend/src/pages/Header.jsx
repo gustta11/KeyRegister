a73bbf0b800ff1dbbad16039e5b4549a3bcb64b9
CarlosGbr11 <carlos157759@gmail.com>
2024-06-03 20:13:30 -0300

    Update Header.jsx

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -1,9 +1,15 @@
 import './Header.css'
+import { Link } from 'react-router-dom';
+import Voltar from '../imagens/icone_voltar.png';
+
 
 const Header = () => {
     return (
 
         <div className="titulo-header">
+            <div className='botao_voltar'>
+            <Link to='/'><img src={Voltar} alt="voltar" /></Link>
+            </div>
             <h1>Key Register</h1>
         </div>
 
