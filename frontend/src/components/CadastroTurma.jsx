be2e0f07fb34d932b88014f0e6659a5958240b34
CarlosGbr11 <carlos157759@gmail.com>
2024-06-03 21:02:26 -0300

    Atualizando Cadastros

--- frontend/src/components/CadastroTurma.jsx
+++ frontend/src/components/CadastroTurma.jsx
@@ -1,12 +1,77 @@
-import React from "react";
+import { useState } from "react";
 
-const CadastroTurma = () => {
+function CadastroTurma () {
 
-  return (
-    <div>
-      <h1>Cadastro Turma</h1>
+const [formValores, setFormValores] = useState({
+  nome: '',
+  dataNascimento: '',
+  genero: '',
+  cpf: '',
+  telefone: '',
+});
+
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  setFormValores(prevState => ({
+    ...prevState,
+    [name]: value
+  }));
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    console.log("Dados a serem enviados: ", formValores);
+    const response = await fetch('http://localhost:3000/pessoas', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(formValores)
+    });
+
+    const json = await response.json();
+    console.log(response)
+    console.log(json);
+
+  } catch (err) {
+    console.error("Erro ao enviar", err)
+  }
+};
+
+return (
+
+  <form onSubmit={handleSubmit}>
+
+    <div className='container-create'>
+      <div className='formulario'>
+        <label>
+          Nome
+          <input type="text" name="nome" value={formValores.nome} onChange={handleChange} />
+        </label>
+        <label>
+          Data de Nascimento
+          <input type="date" name="dataNascimento" value={formValores.dataNascimento} onChange={handleChange} />
+        </label>
+        <label>
+          Gênero
+          <input type="text" name="genero" value={formValores.genero} onChange={handleChange} />
+        </label>
+        <label>
+          CPF
+          <input type="text" name="cpf" value={formValores.cpf} onChange={handleChange} />
+        </label>
+        <label>
+          Telefone
+          <input type="text" name="telefone" value={formValores.telefone} onChange={handleChange} />
+        </label>
+
+        <button type='submit'> CADASTRAR </button>
+
+      </div>
     </div>
-  );
+  </form>
+)
 }
-
 export default CadastroTurma;
--- frontend/src/components/Visualizar.jsx
+++ frontend/src/components/Visualizar.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Visualizar = () => {
+function Visualizar() {
+
+  const [consultaDados, setconsultaDados] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3000/pessoas', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const data = await response.json();
+      setconsultaDados(data);
+    } catch (err) {
+      console.error("Erro ao buscar dados no banco", err);
+    }
+  };
+
+  useEffect(() => {
+    handleSubmit(); 
+  }, []);
 
   return (
-    <div>
-      <h1>Visualizar</h1>
+    <div className="read-container">
+      <form onSubmit={handleSubmit} className="read-form">
+        <button type="submit" className="read-button">Ler dados do banco</button>
+      </form>
+
+      <ol className="read-list">
+        {consultaDados.map((linha, index) => (
+          <li key={index} className="read-dados">
+            <div className="dados">Nome: {linha.nome}</div>
+            <div className="dados">Data Nascimento: {linha.dataNascimento}</div>
+            <div className="dados">Genero: {linha.genero}</div>
+            <div className="dados">CPF: {linha.cpf}</div>
+            <div className="dados">Telefone: {linha.telefone}</div>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
