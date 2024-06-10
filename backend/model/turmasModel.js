bb5528095a16afcc95bc0a15e27a01ba835991ee
CarlosGbr11 <carlos157759@gmail.com>
2024-06-09 23:37:01 -0300

    Update turmasModel.js

--- backend/model/turmasModel.js
+++ backend/model/turmasModel.js
@@ -4,11 +4,11 @@ import connection from "../config/Conecta.js";
 export function read(callback){
     connection.query('SELECT * from turmas WHERE ACTIVO_TURMAS = 1', callback)
 }
-export function create(nome_turma,turno,callback){
-    connection.query('INSERT INTO turmas (NOME_TURMA, TURNO, ACTIVO_TURMAS) VALUES(?,?,1)', [nome_turma,turno], callback)
+export function create(nome_turma,callback){
+    connection.query('INSERT INTO turmas (NOME_TURMA, ACTIVO_TURMAS) VALUES(?,1)', [nome_turma], callback)
 }
-export function update(nome_turma,turno,id, callback){
-    connection.query('UPDATE turmas SET NOME_TURMA = ?, ACTIVO_TURMAS = 1 WHERE COD_TURMA = ?', [nome_turma,turno,id], callback)
+export function update(nome_turma,id, callback){
+    connection.query('UPDATE turmas SET NOME_TURMA = ?, ACTIVO_TURMAS = 1 WHERE COD_TURMA = ?', [nome_turma,id], callback)
 }
 export function deleteTurms(id, callback){
     connection.query('UPDATE  turmas SET ACTIVO_TURMAS = 0 WHERE COD_TURMA = ?', [id], callback)
