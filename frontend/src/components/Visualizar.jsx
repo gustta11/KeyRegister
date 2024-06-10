e1573a91b3e9d87b8ae80d888ad96f2c9d274c61
CarlosGbr11 <carlos157759@gmail.com>
2024-06-10 00:14:57 -0300

    Update Visualizar.jsx

--- frontend/src/components/Visualizar.jsx
+++ frontend/src/components/Visualizar.jsx
@@ -1,78 +1,45 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import Voltar from '../imagens/icone_voltar.png';
-import './cadastro.css';
+import './CadastroDisciplina';
 
 function Visualizar() {
-  const [disciplinas, setDisciplinas] = useState([]);
-  const [professores, setProfessores] = useState([]);
-  const [salas, setSalas] = useState([]);
-  const [turmas, setTurmas] = useState([]);
 
-  const fetchData = async (url, setState) => {
+  const [consultaDados, setconsultaDados] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
     try {
-      const response = await fetch(url, {
+      const response = await fetch('http://localhost:3000/disciplinas', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json'
         }
       });
+
       const data = await response.json();
-      setState(data);
+      setconsultaDados(data);
     } catch (err) {
-      console.error(`Erro ao buscar dados em ${url}`, err);
+      console.error("Erro ao buscar dados no banco", err);
     }
   };
 
   useEffect(() => {
-    fetchData('http://localhost:3000/disciplinas', setDisciplinas);
-    fetchData('http://localhost:3000/professor', setProfessores);
-    fetchData('http://localhost:3000/salas', setSalas);
-    fetchData('http://localhost:3000/turmas', setTurmas);
+    handleSubmit(); 
   }, []);
 
   return (
-    <div className="container">
-      <div className='voltar'>
-        <Link to='/'><img src={Voltar} alt="voltar" /></Link>
-      </div>
-      <div className="data-section">
-        <h2>Professores</h2>
-        <ul>
-          {professores.map((professor, index) => (
-            <li key={index}>
-              Nome: {professor.nome}, Matrícula: {professor.matricula}
-            </li>
-          ))}
-        </ul>
+    <div className="read-container">
+      <form onSubmit={handleSubmit} className="read-form">
+        <button type="submit" className="read-button">Ler dados do banco</button>
+      </form>
 
-        <h2>Salas</h2>
-        <ul>
-          {salas.map((sala, index) => (
-            <li key={index}>
-              {sala.nome_sala}
-            </li>
-          ))}
-        </ul>
-
-        <h2>Disciplinas</h2>
-        <ul>
-          {disciplinas.map((disciplina, index) => (
-            <li key={index}>
-              {disciplina.nome_disciplina}
-            </li>
-          ))}
-        </ul>
-
-        <h2>Turmas</h2>
-        <ul>
-          {turmas.map((turma, index) => (
-            <li key={index}>
-              {turma.nome_turma}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ol className="read-list">
+        {consultaDados.map((linha, index) => (
+          <li key={index} className="read-dados">
+            <div className="dados">nome_disciplina: {linha.nome_disciplina}</div>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
